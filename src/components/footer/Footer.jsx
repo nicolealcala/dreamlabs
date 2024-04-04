166960f00c02f5a8dc1454adea0b54eeb25d39b8
Nicole Alcala <nicolealcalq@gmail.com>
2024-04-04 14:41:23 +0800

    footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="row mx-0 pb-3">
+      <div className="col-md-6 txt-size-small txt-weight-mid text-md-start text-center">
+        <a
+          href="https://nicolealcala.github.io/"
+          target="_blank"
+          className="portfolio"
+        >
+          nicolealcala.github.io
+        </a>
+      </div>
+      <div className="col-md-6 txt-color-soft txt-size-small text-md-end text-center">
+        Copyright &copy; 2024. DreamLabs. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
