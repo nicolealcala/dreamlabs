04cb9799862e90d926f7365eb0e9100c7ccea854
Nicole Alcala <nicolealcalq@gmail.com>
2024-04-05 15:22:27 +0800

    updated date options

--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./postcard.module.css";
-import { options } from "@/lib/utils";
 
 const truncateContent = (content, maxLength) => {
   if (content.length > maxLength) {
@@ -27,7 +26,11 @@ const PostCard = ({ item }) => {
       </div>
       <div>
         <span className={styles.date}>
-          {new Date(item.createdAt).toLocaleDateString("en-US", options)}
+          {new Date(item.createdAt).toLocaleDateString("en-US", {
+            month: "long",
+            day: "numeric",
+            year: "numeric",
+          })}
         </span>
         <h6 className={styles.title} title={item?.title}>
           {truncateContent(item?.title, 50)}
