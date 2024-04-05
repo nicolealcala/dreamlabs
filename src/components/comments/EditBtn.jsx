5f53cd7c6a63531850b33694c22b176e803abe97
Nicole Alcala <nicolealcalq@gmail.com>
2024-04-05 16:41:39 +0800

    edit comment functionality

---
+++ src/components/comments/EditBtn.jsx
@@ -0,0 +1,25 @@
+"use client";
+import { useState } from "react";
+import styles from "./comment.module.css";
+import EditModal from "./EditCommentModal";
+const EditBtn = ({ comment }) => {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      <button
+        className="bg-transparent border-0 txt-size-small me-2"
+        id={styles.edit}
+        onClick={() => setShowModal((m) => !m)}
+      >
+        Edit
+      </button>
+      <EditModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        comment={comment}
+      />
+    </>
+  );
+};
+
+export default EditBtn;
---
+++ src/components/comments/EditCommentModal.jsx
@@ -0,0 +1,95 @@
+"use client";
+import { useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+import { updateComment } from "@/lib/actions";
+import Swal from "sweetalert2";
+import Tiptap from "../tiptap/Tiptap";
+
+const EditModal = ({ showModal, setShowModal, comment }) => {
+  const [content, setContent] = useState(comment?.content);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    try {
+      await updateComment(comment._id, formData);
+      Swal.fire({
+        title: "Success",
+        html: "<em>Your comment has been updated!</em>",
+        icon: "success",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      handleClose();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const confirmClose = () => {
+    if (content) {
+      Swal.fire({
+        title: "Are you sure?",
+        html: "<em>Your changes won't be saved.</em>",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Discard Changes",
+        cancelButtonText: "Cancel",
+        reverseButtons: true,
+      }).then((result) => {
+        if (result.isConfirmed) {
+          handleClose();
+        }
+      });
+    } else {
+      handleClose();
+    }
+  };
+
+  const handleClose = () => {
+    setContent(comment?.content);
+    setShowModal(false);
+  };
+  return (
+    <Modal
+      show={showModal}
+      onHide={confirmClose}
+      backdrop="static"
+      size="lg"
+      data-bs-theme="dark"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title className="fs-5">Edit Comment</Modal.Title>
+      </Modal.Header>
+      <Form onSubmit={handleSubmit}>
+        <Modal.Body>
+          <Form.Group className="mb-3" controlId="content">
+            <Form.Label>Content</Form.Label>
+            <Tiptap
+              text={content}
+              onChange={(c) => setContent(c)}
+              disable={true}
+            />
+            <Form.Control type="hidden" name="content" value={content} />
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={confirmClose}>
+            Discard
+          </Button>
+          <Button
+            className="primary-btn"
+            variant="none"
+            type="submit"
+            disabled={!content}
+          >
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Form>
+    </Modal>
+  );
+};
+
+export default EditModal;
