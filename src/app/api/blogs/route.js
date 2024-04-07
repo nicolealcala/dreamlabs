bd7e6b111f933202e6ec383df646c5c8117f4a50
Nicole Alcala <nicolealcalq@gmail.com>
2024-04-07 15:50:06 +0800

    commented blogs api route

--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -1,13 +1,13 @@
-import { Blog } from "@/lib/models"
-import { connectToDb } from "@/lib/utils"
-import { NextResponse } from "next/server";
+// import { Blog } from "@/lib/models"
+// import { connectToDb } from "@/lib/utils"
+// import { NextResponse } from "next/server";
 
-export const GET = async (req) => {
-    try {
-        connectToDb()
-        const blogs = Blog.find();
-        return NextResponse.json(blogs);
-    } catch (err) {
-        throw new Error('Failed to fetch posts.')
-    }
-}
+// export const GET = async (req) => {
+//     try {
+//         connectToDb()
+//         const blogs = Blog.find();
+//         return NextResponse.json(blogs);
+//     } catch (err) {
+//         throw new Error('Failed to fetch posts.')
+//     }
+// }
