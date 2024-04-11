b4b6a545146c6bbd74ccc93a85527f3cb408f308
Nicole Alcala <nicolealcalq@gmail.com>
2024-04-11 14:58:09 +0800

    commentsBox skeleton

--- src/components/comments/Skeleton.jsx
+++ src/components/comments/Skeleton.jsx
@@ -6,10 +6,22 @@ const Skeleton = () => {
         style={{ width: "35px", height: "35px" }}
       ></div>
 
-      <div
-        className="ms-2 rounded-2 placeholder placeholder-wave"
-        style={{ width: "250px", height: "80px" }}
-      ></div>
+      <div className="ms-2 d-flex flex-column">
+        <div
+          className="rounded-2 placeholder placeholder-wave"
+          style={{ width: "250px", height: "50px" }}
+        ></div>
+        <div className="d-flex mt-2 justify-content-between">
+          <div
+            className="rounded-2 placeholder placeholder-wave"
+            style={{ width: "40px", height: "10px" }}
+          ></div>
+          <div
+            className="rounded-2 placeholder placeholder-wave"
+            style={{ width: "200px", height: "10px" }}
+          ></div>
+        </div>
+      </div>
     </div>
   );
 };
